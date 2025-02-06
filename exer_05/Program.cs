706a99e3fbfb14f8e4d5c1b832e7d6bd25187484
Alex Sandro dos Santos <158177491+AlexSantos18@users.noreply.github.com>
2025-02-06 14:07:04 -0300

    Compilaçao em C#

---
+++ exer_05/Program.cs
@@ -0,0 +1,4 @@
+﻿int n = 0;
+Console.Write("Digite um numero inteiro: ");
+n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Analizando o numero digitado {n} seu antessesor foi {--n} e o seu possesor foi {(++n)+1}");
---
+++ exer_06/Program.cs
@@ -0,0 +1,8 @@
+﻿int n = 0;
+//double resultado = 0; declarar a variavel double
+Console.Write("digite um numero: ");
+n = int.Parse(Console.ReadLine());
+Console.WriteLine($"O dobro do numero {n} é {n*2}");
+Console.WriteLine($"O triplo do numero {n} é {n*3}");
+//resultado = Math.Sqrt(n); recebe a raiz quadrada da variavel 
+Console.WriteLine($"A raiz quadrada do numero {n} é {Math.Sqrt(n)}"); // pode se fazer direto na expressao 
