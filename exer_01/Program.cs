7e5df7314696c822d8c1a5a65a87caade798a12f
Alex Sandro dos Santos <158177491+AlexSantos18@users.noreply.github.com>
2025-02-06 14:04:37 -0300

    Compilaçao em C#

---
+++ exer_01/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool Type(double n3) => throw new NotImplementedException();
+
+        double n1, n2, n3;
+        n3 = 0;
+        Console.Write("Digite um numero: ");
+        n1 = double.Parse(Console.ReadLine());
+        Console.Write("Digite outro numero: ");
+        n2 = double.Parse(Console.ReadLine());
+        n3 = n1 + n2;
+        Console.WriteLine($"A soma de {n1} e {n2} é {n3}");
+        Console.WriteLine((n3));
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
